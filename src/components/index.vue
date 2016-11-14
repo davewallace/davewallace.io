37213b420d511f4e6bee20df596ab7ffc306a2b0
Dave Wallace <danjah@gmail.com>
2016-11-14 16:06:58 +1300

    Inelegant media query implementation of responsive panels.

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,27 +1,115 @@
 <template>
-  <div>
 
-    <!--
-      Replace following "div" with
-      "<router-view class="layout-view">" component
-      if using subRoutes
-    -->
+  <!--
+    Replace following "div" with
+    "<router-view class="layout-view">" component
+    if using subRoutes
+  -->
 
-     <div class="layout-view">
-      <h1>Index</h1>
-     </div>
+   <div>
+    <h1>Index</h1>
+    <div v-for="project in projectData" class="project"></div>
+   </div>
 
-  </div>
 </template>
 
 <script>
 import Quasar from 'quasar'
 
+// TODO: mock browser, language & tool listings
+let projectData = [
+  /* eslint-disable */
+  // JSON format for future-port to API response
+  {
+    "meta":{
+      "name": "Saab",
+      "tags": ["advert", "microsite", "html", "showcase"]
+    },
+    "media": {
+
+    }
+  },
+  {
+    "meta":{
+      "name": "Computer Associates",
+      "tags": ["advert", "banner", "flash", "showcase"]
+    },
+    "media": {
+
+    }
+  },
+  {
+    "meta":{
+      "name": "ASADA Anti-Doping",
+      "tags": ["learning", "html", "responsive", "showcase"]
+    },
+    "media": {
+
+    }
+  },
+  {
+    "meta":{
+      "name": "Education New Zealand",
+      "tags": ["learning", "system", "responsive", "showcase"]
+    },
+    "media": {
+
+    }
+  },
+  {
+    "meta":{
+      "name": "Kiwibank Blueprint",
+      "tags": ["communication", "html", "parallax", "responsive", "showcase"]
+    },
+    "media": {
+
+    }
+  },
+  {
+    "meta":{
+      "name": "Grunt workflow",
+      "tags": ["tooling", "javascript", "build", "team", "internal"]
+    },
+    "media": {
+
+    }
+  },
+  {
+    "meta":{
+      "name": "Git workflow",
+      "tags": ["tooling", "git", "team", "internal"]
+    },
+    "media": {
+
+    }
+  },
+  {
+    "meta":{
+      "name": "React",
+      "tags": ["javascript", "prototype"]
+    },
+    "media": {
+
+    }
+  },
+  {
+    "meta":{
+      "name": "Vue",
+      "tags": ["javascript", "prototype"]
+    },
+    "media": {
+
+    }
+  }
+  /* eslint-disable */
+]
+
 export default {
   name: 'index',
   data () {
     return {
-      quasarVersion: Quasar.version
+      quasarVersion: Quasar.version,
+      projectData: projectData
     }
   },
   computed: {
@@ -42,5 +130,62 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+
+// TODO: not DRY, find alternative, likely JS to add/remove classes from DOM
+
+.project {
+  // 3 columns
+
+  box-sizing: border-box;
+  display: inline-block;
+  margin: 0 10px 10px 0;
+
+  // sizing
+  width: calc(33.3% - 7px);
+  height: 300px;
+
+  &:nth-child(3n+1) {
+    width: calc(33.3% - 5px);
+    margin-right:0px;
+  }
+
+  background:pink;
+}
+
+@media (max-width:1024px) {
+  // 2 columns
+
+  .project {
+    background:lime;
+
+    // sizing
+    &:nth-child(1n) {
+      width: calc(50% - 5px);
+    }
+    height: 350px;
+    margin: 0 10px 10px 0;
+
+    &:nth-child(odd) {
+      margin-right:0px;
+    }
+    &:nth-child(even) {
+      margin-right:10px;
+    }
+
+  }
+}
+@media (max-width:640px) {
+  // single column
+
+  .project {
+    background:cyan;
+
+    // sizing
+    &:nth-child(1n) {
+      width: 100%;
+    }
+    margin: 0 0 10px 0;
+  }
+}
 </style>
