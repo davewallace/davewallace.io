2c7e2b2862b44b77145c417080ccc022e9bea9c5
Dave Wallace <contact@davewallace.io>
2018-03-05 22:33:04 +1300

    Replace build structure and initial build with vue-cli output.

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+
+  /**
+   * All imports and contextual SCSS must be contained within this single style tag
+   */
+
+  @import "style/variables.scss";
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: $color__text-base;
+  }
+</style>
