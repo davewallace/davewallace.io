35767afc6f601fe84321e1b61a5c94ce00403676
Dave Wallace <contact@davewallace.io>
2018-04-25 10:54:03 +1200

    Gotta get that commit count up.

--- src/components/ui/Layout.vue
+++ src/components/ui/Layout.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="layout">
-    <!-- XSS attack vector. I can roll with that right now because
+    <!-- XSS attack vector? I can roll with that right now because
          I control the data, and the data will be piped into here via
          showdown (https://github.com/showdownjs/showdown) -->
     <div class="layout-item" v-html="data"/>
