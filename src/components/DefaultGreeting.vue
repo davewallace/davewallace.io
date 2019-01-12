5d0bced36745afe3ed149697b98396af73e41e21
Dave Wallace <contact@davewallace.io>
2019-01-12 16:37:29 +1300

    Update default view page greeting styles and content.

--- src/components/DefaultGreeting.vue
+++ src/components/DefaultGreeting.vue
@@ -1,23 +1,42 @@
 <style scoped lang="scss">
-h1 {
-  margin-bottom: 0;
-  padding-top: 2em;
-  font-size: 2.4em;
 
-  span {
-    font-size: 1.4rem;
+@import "../style/variables.scss";
+@import "../style/reset.scss";
+@import "../style/utility.scss";
+
+h1 {
+  display: inline-block;
+  padding-top: 2em;
+  @media (max-width: 320px) {
+    padding-top: 1em;
   }
+
+  font-size: 4.8em;
+  line-height: 1.2rem;
+  font-family: $font_title--main;
 }
+p {
+  display: inline-block;
+  position: relative;
+  left: 0.1em;
+  margin-bottom: 10px;
+  font-family: $font_title--main;
+  font-size: 1.8rem;
+  line-height: 1.1em;
+}
+
 h2 {
   margin-bottom: 40px;
   font-size: 1.1rem;
   line-height: 1.2rem;
+  font-weight: normal;
 }
 </style>
 
 <template>
   <div>
-    <h1>{{ greeting_primary }}<span>{{ greeting_secondary }}</span></h1>
+    <h1>{{ greeting_primary }}</h1>
+    <p>{{ greeting_secondary }}</p>
     <h2>{{ greeting_tertiary }}</h2>
   </div>
 </template>
@@ -27,8 +46,8 @@ export default {
   name: 'DefaultGreeting',
   data () {
     return {
-      greeting_primary: 'Hello',
-      greeting_secondary: ', and welcome.',
+      greeting_primary: 'Hello,',
+      greeting_secondary: 'and welcome.',
       greeting_tertiary: 'You\'ve opened my portfolio, thanks. Select the work you\'d like to see...'
     }
   },
