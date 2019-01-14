9c0b0791467d865be5cacd782bbc768a28d36942
Dave Wallace <contact@davewallace.io>
2019-01-14 20:07:40 +1300

    Add blockquote component

---
+++ src/components/ui/Blockquote.vue
@@ -0,0 +1,44 @@
+<template>
+  <blockquote>
+
+      <slot name="blockquote_body">Blockquote text</slot>
+
+      <cite class="blockquote_citation">
+        <slot name="blockquote_citation">Blockquote citation</slot>
+      </cite>
+  </blockquote>
+</template>
+
+<style scoped lang="scss">
+
+@import "../../style/variables.scss";
+@import "../../style/reset.scss";
+
+blockquote {
+  margin: 0;
+  padding: 10px 10px 1px 20px; // margin-collapsing when bottom-pad set to 0px
+  border-left: 3px solid $color__base--green;
+  color: $color__base--green;
+  font-size: $font__size--large;
+}
+blockquote + blockquote { // limited by markdown output, I'm using a 2nd blockquote as the quotee
+  padding-top: 0px;
+  text-align: right;
+  font-style: italic;
+  color: $color__base--reverse;
+}
+</style>
+
+<script>
+
+export default {
+  name: 'Blockquote',
+  data () {
+    return {
+    }
+  },
+  props: [
+    'type'
+  ]
+}
+</script>
