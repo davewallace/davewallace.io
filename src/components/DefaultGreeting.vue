a7b8bc977bfc0aa8fe524494745842b1bcc37ae4
Dave Wallace <contact@davewallace.io>
2020-02-02 00:27:13 +1300

    Simplify selectors, markup and base vars

--- src/components/DefaultGreeting.vue
+++ src/components/DefaultGreeting.vue
@@ -1,61 +1,43 @@
+<template>
+  <header>
+    <h1>Hello<span>, and welcome</span></h1>
+    <p>My name is Dave Wallace, and you've opened my portfolio - thanks. I'm a web developer and UX consultant.</p>
+    <p>A selection of my previous work is available below, you can filter it using the labels...</p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'DefaultGreeting'
+}
+</script>
+
 <style scoped lang="scss">
 
 @import "../style/variables.scss";
 @import "../style/reset.scss";
 @import "../style/utility.scss";
 
-h1 {
-  display: inline-block;
-  padding-top: 2em;
+header {
+  margin-top: 40px;
+  margin-bottom: 40px;
   @media (max-width: 320px) {
-    padding-top: 1em;
+    margin-top: 20px;
   }
-
+}
+h1 {
   font-size: 4.8em;
-  line-height: 1.2rem;
+  line-height: 4.8rem;
   font-family: $font_title--main;
+
+  span {
+    font-size: 1.8rem;
+    line-height: 1.8em;
+  }
 }
 p {
-  display: inline-block;
-  position: relative;
-  left: 0.1em;
-  margin-bottom: 10px;
-  font-family: $font_title--main;
-  font-size: 1.8rem;
-  line-height: 1.1em;
-}
-
-h2 {
-  margin-bottom: 40px;
   font-size: 1.1rem;
   line-height: 1.2rem;
   font-weight: normal;
 }
 </style>
-
-<template>
-  <div>
-    <h1>{{ greeting_primary }}</h1>
-    <p>{{ greeting_secondary }}</p>
-    <h2>{{ greeting_tertiary }}</h2>
-  </div>
-</template>
-
-<script>
-export default {
-  name: 'DefaultGreeting',
-  data () {
-    return {
-      greeting_primary: 'Hello',
-      greeting_secondary: ', and welcome.',
-      greeting_tertiary: 'You\'ve opened my portfolio, thanks. Select the work you\'d like to see...'
-    }
-  },
-
-  /**
-   * Lifecycle methods
-   **/
-  created: function () {
-  }
-}
-</script>
